b530d8dadccd3507c6a3a8e81f1c234ec2170f27
Yuya Hochi <yuya1charo@gmail.com>
2022-10-31 13:27:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css";
 
 function App() {
+  const body = document.body;
+  const html = document.documentElement;
+  const [status, setStatus] = useState(0);
+  const [height, setHeight] = useState(Math.max( body.scrollHeight, body.offsetHeight, 
+    html.clientHeight, html.scrollHeight, html.offsetHeight ) - window.innerHeight);
+  
+    useEffect (() => {
+    window.addEventListener("scroll", handleStatus, {passive: true});
+    return () => {
+      window.removeEventListener("scroll", handleStatus);
+    }
+  }, [])
+
+  useEffect(() => {
+    document.getElementById("status").style.width =  status / height * 100+ "%";
+  }, [status])
+
+  useEffect(() => {
+    setHeight(Math.max( body.scrollHeight, body.offsetHeight, 
+      html.clientHeight, html.scrollHeight, html.offsetHeight ) - window.innerHeight);
+  } , [window.innerHeight])
+
+  const handleStatus = () => {
+    const positionY = window.pageYOffset;
+    setStatus(positionY);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class ="progress-bar">
+        <div className="status" id = "status">{Math.round(status / height * 100)}</div>
+      </div>
+      <div className="pages">
+        <div className="page1"></div>
+        <div className="page2"></div>
+        <div className="page3"></div>
+        <div className="page4"></div>
+      </div>
+      
     </div>
   );
 }
